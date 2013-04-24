437b9d044b106f87c1de8d7f647e1c502ef1aaab
Alexander Standke <xanderstrike@gmail.com>
2013-04-23 21:21:13 -0700

    rest of URLS and routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
-	before_filter :authenticate_user!
+	# before_filter :authenticate_user!
 	
   def index
     @users = User.all
+  end
+
+  def faq
+  end
+
+  def rules
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails3BootstrapDeviseCancan::Application.routes.draw do
-  resources :comments
-  resources :posts
-
-  match '/links' => 'posts#links'
-  match '/requests' => 'posts#requests'
-  match '/forums' => 'posts#forums'
+  
 
   authenticated :user do
     root :to => 'home#index'
+    
+    resources :users
+    resources :comments
+	  resources :posts
+
+	  match '/faq' => 'home#faq'
+	  match '/rules' => 'home#rules'
+
+	  match '/links' => 'posts#links'
+	  match '/requests' => 'posts#requests'
+	  match '/forums' => 'posts#forums'
   end
   root :to => "home#index"
   devise_for :users
   resources :users
+
+  # match "*path" => redirect("/")
 end
